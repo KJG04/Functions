3ff08e6ac1e821710badb90cce0dc892e53f325e
김진근 <freedom7113@gmail.com>
2021-11-04 16:55:00 +0900

    ⚡️ :: (#1) 경과 시간 컴포넌트 만듬

---
+++ functions/src/components/ElapsedTime/ElapsedTime.tsx
@@ -0,0 +1,36 @@
+import { useLayoutEffect, useState } from "react";
+
+interface PropsType {
+  interval: number;
+  from: Date;
+}
+
+const ElapsedTime = ({ interval, from }: PropsType): JSX.Element => {
+  const [timeString, setTimeString] = useState<string>("00:00:00");
+
+  const onInterval = () => {
+    const now = new Date();
+
+    const seconds = Math.floor((now.getTime() - from.getTime()) / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    setTimeString(
+      `${hours.toString().padStart(2, "0")}:${(minutes % 60).toString().padStart(2, "0")}:${(
+        seconds % 60
+      )
+        .toString()
+        .padStart(2, "0")}`
+    );
+  };
+
+  useLayoutEffect(() => {
+    const timeInterval = setInterval(onInterval, interval);
+
+    return () => clearInterval(timeInterval);
+  }, [from]);
+
+  return <span>{timeString}</span>;
+};
+
+export default ElapsedTime;
