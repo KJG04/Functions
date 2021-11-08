6343a140944463bd32533121c1e1247a12ce697e
김진근 <freedom7113@gmail.com>
2021-11-08 18:33:31 +0900

    ⚡️ :: (#6) 퍼블리싱 완료

--- functions/src/components/Main/Main.tsx
+++ functions/src/components/Main/Main.tsx
@@ -1,7 +1,32 @@
 import * as S from "./styles";
 
 const Main = (): JSX.Element => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.TitleContainer>
+        <S.TitleContainerInner>
+          <S.NoDecoLink to="/">
+            <S.Title>Minesweeper</S.Title>
+          </S.NoDecoLink>
+          <S.NoDecoLink to="/">
+            <S.Title>Logic Gate </S.Title>
+          </S.NoDecoLink>
+          <S.NoDecoLink to="/">
+            <S.Title>Finding a way </S.Title>
+          </S.NoDecoLink>
+          <S.NoDecoLink to="/">
+            <S.Title>Heap tree </S.Title>
+          </S.NoDecoLink>
+          <S.NoDecoLink to="/">
+            <S.Title>Dice </S.Title>
+          </S.NoDecoLink>
+          <S.NoDecoLink to="/">
+            <S.Title>Contact me</S.Title>
+          </S.NoDecoLink>
+        </S.TitleContainerInner>
+      </S.TitleContainer>
+    </S.Container>
+  );
 };
 
 export default Main;
--- functions/src/components/Main/styles.ts
+++ functions/src/components/Main/styles.ts
@@ -1,11 +1,36 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { color } from "../Minesweeper";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  padding: 48px;
 `;
 
 export const Title = styled.div`
-    
+  font-family: "Montserrat", sans-serif;
+  font-size: 72px;
+  color: ${color.black};
+  text-align: right;
+  user-select: none;
+`;
+
+export const TitleContainer = styled.div`
+  padding: 48px;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: right;
+  display: flex;
+  box-sizing: border-box;
+`;
+
+export const TitleContainerInner = styled.div`
+  display: flex;
+  row-gap: 24px;
+  flex-direction: column;
+`;
+
+export const NoDecoLink = styled(Link)`
+  text-decoration: none;
 `;
