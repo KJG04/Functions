83db6d4f90b50ef9d4c63971cc4d8199077e0e2d
김진근 <freedom7113@gmail.com>
2021-11-08 18:19:20 +0900

    ⚡️ :: (#6) 메인 페이지

---
+++ functions/src/components/Main/Main.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const Main = (): JSX.Element => {
+  return <S.Container></S.Container>;
+};
+
+export default Main;
---
+++ functions/src/components/Main/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 48px;
+`;
+
+export const Title = styled.div`
+    
+`;
