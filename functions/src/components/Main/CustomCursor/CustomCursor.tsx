f213aa7652c380124f24257c58e4e27d8379af52
김진근 <freedom7113@gmail.com>
2021-11-16 18:07:14 +0900

    ⚡️ :: (#7) 마우스 누르고 있을때 커서 변경

--- functions/src/components/Main/CustomCursor/CustomCursor.tsx
+++ functions/src/components/Main/CustomCursor/CustomCursor.tsx
@@ -1,6 +1,5 @@
 import { useLayoutEffect, useRef, useState } from "react";
 import * as S from "./styles";
-import gsap, { Power4 } from "gsap";
 
 interface PropsType {
   color: string;
@@ -8,9 +7,11 @@ interface PropsType {
 }
 const CustomCursor = ({ color, scale }: PropsType): JSX.Element => {
   const cursor = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState<number>(1);
 
   const onMove = (e: MouseEvent) => {
     const { x, y } = e;
+
     if (cursor && cursor.current) {
       cursor.current.style.top = `${y}px`;
       cursor.current.style.left = `${x}px`;
@@ -19,12 +20,20 @@ const CustomCursor = ({ color, scale }: PropsType): JSX.Element => {
 
   useLayoutEffect(() => {
     window.addEventListener("mousemove", onMove);
-  }, []);
+    window.addEventListener("mousedown", () => setOffset(0.5));
+    window.addEventListener("mouseup", () => setOffset(1));
+
+    return () => {
+      window.removeEventListener("mousemove", onMove);
+      window.removeEventListener("mousedown", () => setOffset(0.5));
+      window.removeEventListener("mouseup", () => setOffset(1));
+    };
+  }, [scale]);
 
   return (
     <S.Container>
       <S.Inner>
-        <S.Cursor color={color} scale={scale} ref={cursor} />
+        <S.Cursor color={color} scale={scale * offset} ref={cursor} />
       </S.Inner>
     </S.Container>
   );
