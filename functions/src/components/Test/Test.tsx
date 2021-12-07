ef8c93766fb231bc2a08330d5779a2c427979735
김진근 <freedom7113@gmail.com>
2021-12-07 17:17:12 +0900

    :coffin: :: (#9) clear test

--- functions/src/components/Test/Test.tsx
+++ functions/src/components/Test/Test.tsx
@@ -1,20 +1,8 @@
 import { useState } from "react";
 import * as S from "./styles";
+
 const Test = (): JSX.Element => {
-  const [bool, setBool] = useState<boolean>(false);
-  return (
-    <>
-      <S.Cell
-        onClick={() => {
-          setBool(!bool);
-        }}
-        bool={bool}
-        axisX={1}
-        axisY={1}
-        sign={-1}
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default Test;
