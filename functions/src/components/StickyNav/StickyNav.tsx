fae3eb78f8dff5601fb5bbb205126c1200eea8a0
김진근 <freedom7113@gmail.com>
2021-12-09 16:34:47 +0900

    ⚡️ :: (#12) write stciky nav component

--- functions/src/components/StickyNav/StickyNav.tsx
+++ functions/src/components/StickyNav/StickyNav.tsx
@@ -1,10 +1,53 @@
+import { useRef } from "react";
+import * as S from "./styles";
 
-interface PropsType{
-    text: string;
+interface PropsType {
+  text: string;
+  onClick?: () => void;
 }
 
-const StickyNav = () => {
+const StickyNav = ({ text, onClick }: PropsType) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLDivElement>(null);
 
-}
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!containerRef.current || !textRef.current) {
+      return;
+    }
+
+    const container = containerRef.current;
+    const text = textRef.current;
+    const { offsetX: x, offsetY: y } = e.nativeEvent;
+    const { offsetWidth: width, offsetHeight: height } = container;
+
+    const move = 25;
+    const xMove = (x / width) * (move * 2) - move;
+    const yMove = (y / height) * (move * 2) - move;
+
+    text.style.transform = `translate(${xMove}px, ${yMove}px)`;
+  };
+
+  const onMouseLeave = () => {
+    if (!textRef.current) {
+      return;
+    }
+    const text = textRef.current;
+
+    text.style.transform = `translate(0px, 0px)`;
+  };
+
+  return (
+    <S.Container>
+      <S.Con
+        ref={containerRef}
+        onClick={onClick}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+      >
+        <S.Text ref={textRef}>{text}</S.Text>
+      </S.Con>
+    </S.Container>
+  );
+};
 
 export default StickyNav;
--- functions/src/components/StickyNav/styles.ts
+++ functions/src/components/StickyNav/styles.ts
@@ -1,1 +1,21 @@
-export {};
+import styled from "@emotion/styled";
+import { font } from "../../style/font";
+
+export const Text = styled.div`
+  transition: transform 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  font: ${font.headline1};
+  cursor: default;
+`;
+
+export const Con = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Container = styled.span`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
