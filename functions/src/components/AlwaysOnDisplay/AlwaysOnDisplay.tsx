5c68192df8e1ad04f77cc6dc7db1ad9b47b10e6b
김진근 <freedom7113@gmail.com>
2021-12-10 13:07:22 +0900

    ⚡️ :: (#12) write AOD component

--- functions/src/components/AlwaysOnDisplay/AlwaysOnDisplay.tsx
+++ functions/src/components/AlwaysOnDisplay/AlwaysOnDisplay.tsx
@@ -1,7 +1,14 @@
-import * as S from './styles';
+import * as S from "./styles";
+import DarkLogo from "../../assets/logos/LogoDark.svg";
+import LightLogo from "../../assets/logos/LogoLight.svg";
 
 const AlwaysOnDisplay = () => {
-
-}
+  return (
+    <S.LogoContainer>
+      <S.Logo src={DarkLogo} isActive={false} />
+      <S.Logo src={LightLogo} isActive={true} />
+    </S.LogoContainer>
+  );
+};
 
 export default AlwaysOnDisplay;
--- functions/src/components/AlwaysOnDisplay/styles.ts
+++ functions/src/components/AlwaysOnDisplay/styles.ts
@@ -1,5 +1,18 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-    
-`
+export const LogoContainer = styled.div`
+  width: 40px;
+  position: absolute;
+  top: 60px;
+  left: 60px;
+`;
+
+export const Logo = styled.img<{ isActive: boolean }>`
+  width: 100%;
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: ${(props) => (props.isActive ? 1 : 0)};
+  transition: opacity 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+`;
