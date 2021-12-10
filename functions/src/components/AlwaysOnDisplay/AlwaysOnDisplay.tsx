daf562a3af5ba77081f19157bb84601ba66c9401
김진근 <freedom7113@gmail.com>
2021-12-10 13:16:25 +0900

    ⚡️ :: (#12) add custom cursor component

--- functions/src/components/AlwaysOnDisplay/AlwaysOnDisplay.tsx
+++ functions/src/components/AlwaysOnDisplay/AlwaysOnDisplay.tsx
@@ -1,13 +1,24 @@
 import * as S from "./styles";
 import DarkLogo from "../../assets/logos/LogoDark.svg";
 import LightLogo from "../../assets/logos/LogoLight.svg";
+import { useState } from "react";
+import CustomCursor from "../Main/CustomCursor/CustomCursor";
+import { color } from "../../style/color";
 
 const AlwaysOnDisplay = () => {
+  const [isHover, setIsHover] = useState(false);
+  const [scale, setScale] = useState(1);
+
   return (
-    <S.LogoContainer>
-      <S.Logo src={DarkLogo} isActive={false} />
-      <S.Logo src={LightLogo} isActive={true} />
-    </S.LogoContainer>
+    <>
+      <S.CursorOuter isActive={isHover}>
+        <CustomCursor color={color.backgroundColor} scale={scale} />
+      </S.CursorOuter>
+      <S.LogoContainer onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+        <S.Logo src={DarkLogo} isActive={false} />
+        <S.Logo src={LightLogo} isActive={true} />
+      </S.LogoContainer>
+    </>
   );
 };
 
