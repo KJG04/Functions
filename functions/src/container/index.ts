a1bf1adec3a3d260dcb6518cbcf1533862b0c4d4
김진근 <freedom7113@gmail.com>
2021-12-16 13:58:26 +0900

    ⚡️ :: (#21) add dice container in route

--- functions/src/container/index.ts
+++ functions/src/container/index.ts
@@ -2,3 +2,4 @@ export { default as MinesweeperContainer } from "./Minesweeper";
 export { default as TestContainer } from "./Test";
 export { default as MainContainer } from "./Main";
 export { default as ContactMeContainer } from "./ContactMe";
+export { default as DiceContainer } from "./Dice";
