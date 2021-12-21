2d1e46da1ca928cd87cb94e6ba3bae486e7cebad
김진근 <freedom7113@gmail.com>
2021-12-21 15:10:46 +0900

    ⚡️ :: (#21) add test code

--- functions/src/components/Test/Test.tsx
+++ functions/src/components/Test/Test.tsx
@@ -1,8 +1,37 @@
 import * as S from "./styles";
 import StickyNav from "../StickyNav/StickyNav";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/cannon";
+import { color } from "../Minesweeper";
+import DiceRender from "../Dice/DiceRender/DiceRender";
+import Plane from "../Main/Plane/Plane";
 
 const Test = () => {
-  return <div />;
+  return (
+    <S.Container>
+      <Canvas
+        mode="concurrent"
+        shadows
+        gl={{ alpha: false }}
+        camera={{ position: [0, -15, 15], fov: 50 }}
+      >
+        <hemisphereLight intensity={1} color={color.backgroundColor} />
+        <spotLight
+          position={[-12, 8, 12]}
+          angle={0.9}
+          penumbra={0.5}
+          intensity={2}
+          castShadow
+          shadow-mapSize-width={256}
+          shadow-mapSize-height={256}
+        />
+        <Physics gravity={[0, 0, -30]}>
+          <Plane color={color.backgroundColor} />
+          <DiceRender />
+        </Physics>
+      </Canvas>
+    </S.Container>
+  );
 };
 
 export default Test;
