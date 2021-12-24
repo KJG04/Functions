176165d1ec4c6de253d94cef678ad867f5adf739
김진근 <freedom7113@gmail.com>
2021-12-24 14:45:47 +0900

    ⚡️ :: (#21) add prev now state in AwesomeTextTransiton

--- functions/src/components/AwesomeTextTransiton/AwesomeTextTransiton.tsx
+++ functions/src/components/AwesomeTextTransiton/AwesomeTextTransiton.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import * as S from "./styles";
+
 interface PropsType {
   text: string;
   style?: React.CSSProperties;
 }
 
+interface TextType {
+  now: string;
+  prev: string;
+}
+
 const AwesomeTextTransiton = ({ text, style }: PropsType) => {
-  return <div style={style}>{text}</div>;
+  const [texts, setTexts] = useState<TextType>({ now: "", prev: "" });
+  const { now, prev } = texts;
+
+  useEffect(() => {
+    setTexts((oldData) => ({ now: text, prev: oldData.now }));
+  }, [text]);
+
+  return (
+    <S.Container>
+      <div style={style}>{now}</div>
+      <div style={style}>{prev}</div>
+    </S.Container>
+  );
 };
 
 export default AwesomeTextTransiton;
