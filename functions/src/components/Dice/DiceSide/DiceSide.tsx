7271e4fc28354a6292d4d00fb615be7a11725928
김진근 <freedom7113@gmail.com>
2021-12-27 11:37:48 +0900

    ⚡️ :: (#21) add dot position

--- functions/src/components/Dice/DiceSide/DiceSide.tsx
+++ functions/src/components/Dice/DiceSide/DiceSide.tsx
@@ -1,6 +1,47 @@
+import * as S from "./styles";
+
+const center = "50%";
+const top = "10%";
+const bottom = "90%";
+const right = "90%";
+const left = "10%";
+
+interface Position {
+  vertical: string;
+  horizontal: string;
+}
+
+const topLeft: Position = {
+  vertical: top,
+  horizontal: left,
+};
+const centerLeft: Position = {
+  vertical: center,
+  horizontal: left,
+};
+const bottomLeft: Position = {
+  vertical: bottom,
+  horizontal: left,
+};
+const centerCenter: Position = {
+  vertical: center,
+  horizontal: center,
+};
+const topRight: Position = {
+  vertical: top,
+  horizontal: right,
+};
+const centerRight: Position = {
+  vertical: center,
+  horizontal: right,
+};
+const bottomRight: Position = {
+  vertical: bottom,
+  horizontal: right,
+};
 
 const DiceSide = () => {
-
-}
+  return <S.Container></S.Container>;
+};
 
 export default DiceSide;
--- functions/src/components/Dice/DiceSide/styles.ts
+++ functions/src/components/Dice/DiceSide/styles.ts
@@ -1,1 +1,21 @@
-export {};
+import styled from "@emotion/styled";
+import { color } from "../../Minesweeper";
+
+export const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  background-color: ${color.white};
+  position: relative;
+`;
+
+export const Dot = styled.div<{ vertical: string; horizontal: string }>`
+  width: 20%;
+  height: 20%;
+  border-radius: 50%;
+  background-color: ${color.black};
+  position: absolute;
+  top: ${(props) => props.vertical};
+  left: ${(props) => props.horizontal};
+  transform: translate(-50%, -50%);
+`;
