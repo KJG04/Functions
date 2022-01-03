0e12fd279851329905a7ab974153ad64c248776f
김진근 <freedom7113@gmail.com>
2022-01-03 10:39:21 +0900

    ⚡️ :: (#28) add logic gate in main router

--- functions/src/container/index.ts
+++ functions/src/container/index.ts
@@ -3,3 +3,4 @@ export { default as TestContainer } from "./Test";
 export { default as MainContainer } from "./Main";
 export { default as ContactMeContainer } from "./ContactMe";
 export { default as DiceContainer } from "./Dice";
+export { default as LogicGateContainer } from "./LogicGate";
--- functions/src/route/MainRouter.tsx
+++ functions/src/route/MainRouter.tsx
@@ -7,6 +7,7 @@ import {
   MainContainer,
   MinesweeperContainer,
   TestContainer,
+  LogicGateContainer,
 } from "../container";
 
 const MainRouter: FC = (): JSX.Element => {
@@ -17,6 +18,7 @@ const MainRouter: FC = (): JSX.Element => {
         <Route path="/minesweeper" element={<MinesweeperContainer />} />
         <Route path="/contact" element={<ContactMeContainer />} />
         <Route path="/dice" element={<DiceContainer />} />
+        <Route path="/logicgate" element={<LogicGateContainer />} />
         <Route path="/test" element={<TestContainer />} />
       </Routes>
       <AlwaysOnDisplay />
