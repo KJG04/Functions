afc1252a8ed36c99059d94738b9d5bd7db0287c9
김진근 <freedom7113@gmail.com>
2022-01-03 12:07:46 +0900

    ⚡️ :: (#28) add index to export

---
+++ functions/src/components/LogicGate/Gates/index.ts
@@ -0,0 +1,7 @@
+export { default as ANDGate } from "./AND";
+export { default as NANDGate } from "./NAND";
+export { default as ORGate } from "./OR";
+export { default as NORGate } from "./NOR";
+export { default as XORGate } from "./XOR";
+export { default as NXORGate } from "./NXOR";
+export { default as NOTGate } from "./NOT";
