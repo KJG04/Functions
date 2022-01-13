a10be3f0288c38ba6e67e20360f60dfa3accc437
김진근 <freedom7113@gmail.com>
2022-01-13 16:21:43 +0900

    ⚡️ :: (#28) draw line

--- functions/src/components/LogicGate/LogicGate.tsx
+++ functions/src/components/LogicGate/LogicGate.tsx
@@ -1,16 +1,22 @@
 import { useLayoutEffect } from "react";
 import { color } from "../Minesweeper";
 import { ANDGate } from "./Gates";
+import * as S from "./styles";
 
 const LogicGate = () => {
   useLayoutEffect(() => {
     document.querySelector("html")!.style.backgroundColor = color.red;
   }, []);
 
+  const onDrag = () => {};
+
   return (
     <>
-      <div>hello world!</div>
-      <ANDGate />
+      <S.Container>
+        <S.PathContainer>
+          <path d="M10,55 C65,55 65,100 120,100" fill="none" stroke="#FFFFFF" stroke-width="3" />
+        </S.PathContainer>
+      </S.Container>
     </>
   );
 };
--- functions/src/components/LogicGate/styles.ts
+++ functions/src/components/LogicGate/styles.ts
@@ -1,1 +1,15 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const PathContainer = styled.svg`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
