042a715a2292e7209c144dd688e075e481163c93
김진근 <freedom7113@gmail.com>
2022-01-25 00:16:18 +0900

    ⚡️ :: (#28) add get context

---
+++ functions/src/context/GateContext.ts
@@ -0,0 +1,43 @@
+import { createContext } from "react";
+
+export type Point = [x: number, y: number];
+
+export interface Dot {
+  id: string;
+  position: Point;
+  connectNode: Node;
+}
+
+export interface Gate {
+  id: string;
+  type: string;
+  position: Point;
+  input1: Dot;
+  input2: Dot;
+  output: Dot;
+}
+
+export interface Node {
+  id: string;
+  dot1: Dot;
+  dot2: Dot;
+  isActive: boolean;
+}
+
+export interface GateContextType {
+  gates: Gate[];
+  nodes: Node[];
+  startNode: (dot: Dot) => void;
+  endNode: (position: Point) => void;
+  addNode: (startDotId: string, endDotId: string) => void;
+  setNodeIsActive: (nodeId: string, isActive: boolean) => void;
+}
+
+export const GateContext = createContext<GateContextType>({
+  gates: [],
+  nodes: [],
+  startNode: () => {},
+  endNode: () => {},
+  addNode: () => {},
+  setNodeIsActive: () => {},
+});
