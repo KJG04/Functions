a7087683913ffb5021e80500ad509b82e97c3a77
김진근 <freedom7113@gmail.com>
2022-01-25 16:23:29 +0900

    ⚡️ :: (#28) on gate create setting dot

--- functions/src/components/LogicGate/Gates/AND/index.tsx
+++ functions/src/components/LogicGate/Gates/AND/index.tsx
@@ -1,16 +1,49 @@
 import * as S from "../styles";
 import ANDBody from "../../../../assets/LogicGateBodies/AND.svg";
-import { FC } from "react";
-import { Gate } from "../../../../context/GateContext";
+import { FC, useEffect, useMemo, useRef } from "react";
+import { Dot, Gate } from "../../../../context/GateContext";
+import useGateContext from "../../../../hooks/useGateConext";
+import uniqueId from "../../../../constance/UniqueId";
 
 const ANDGate: FC<Gate & { index: number }> = ({ position, index }) => {
   const [x, y] = position;
+  const { gates } = useGateContext();
+  const currentGate = useMemo(() => gates[index], [gates, index]);
+
+  const input1Ref = useRef<HTMLDivElement>(null);
+  const input2Ref = useRef<HTMLDivElement>(null);
+  const outputRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (input1Ref.current && input2Ref.current && outputRef.current) {
+      const copyGates = [...gates];
+
+      const { input1, input2, output } = currentGate;
+      const dots = [input1, input2, output];
+      const names = ["input1", "input2", "output"];
+      const refs = [input1Ref.current, input2Ref.current, outputRef.current];
+
+      dots.forEach((value, i) => {
+        const id = value ? value.id : uniqueId();
+
+        const name = names[i] as "input1" | "input2" | "output";
+
+        const { offsetTop, offsetLeft } = refs[i];
+
+        copyGates[index][name] = {
+          id: id,
+          connectNode: null,
+          position: [offsetLeft, offsetTop],
+        };
+      });
+    }
+  }, [currentGate, gates, index]);
 
   return (
     <S.Container style={{ top: `${x}px`, left: `${y}px` }} img={ANDBody}>
-      <S.Dot style={{ top: "15%" }} />
-      <S.Dot style={{ top: "85%" }} />
-      <S.Dot style={{ top: "50%", left: "100%" }} />
+      <S.Dot ref={input1Ref} style={{ top: "15%" }} />
+      <S.Dot ref={input2Ref} style={{ top: "85%" }} />
+      <S.Dot ref={outputRef} style={{ top: "50%", left: "100%" }} />
     </S.Container>
   );
 };
